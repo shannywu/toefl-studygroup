614cb43b8399beb2d585412b4907683a35edf2b4
steven5538 <steven5538@gmail.com>
2016-10-03 16:41:01 +0800

    add facebook login.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_oauth import OAuth
+import config
 
 # app config
 
@@ -12,8 +14,22 @@ app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
+# Facebook Oauth
+oauth = OAuth()
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=config.FACEBOOK_APP_ID,
+    consumer_secret=config.FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
+
 # blueprints
 
 from views.home import home
+from views.login import login
 
 app.register_blueprint(home)
+app.register_blueprint(login)
